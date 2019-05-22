c22a524d6f73e983f35aaf734a2eae78439da475
Max Sydney <mjs364@uclive.ac.nz>
2019-05-22 22:33:15 +1200

    Added auxilliary chart to display uncontrolled data. Added handling for all temperatures to be loaded from socket and switched to array style temp storage. Changed updateChart() to run only when new data is available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,24 +11,25 @@ import { splitMatchedQueriesDsl } from '@angular/core/src/view/util';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit  {
+
+export class AppComponent {
   @ViewChild(BaseChartDirective)
   public chart: BaseChartDirective; // Now you can reference your chart via `this.chart`
-  T1 = 0;
-  T2 = 0;
+  currentTemps = [0, 0, 0, 0, 0];
   setpoint: number;
   flowRate: number;
   deltaT: number;
   elementStatus: number;
-  P_gain: number;
-  I_gain: number;
-  D_gain: number;
+  P_gain = 0;
+  I_gain = 0;
+  D_gain = 0;
   fanState = false;
   flush = false;
   filter = false;
   Qdot: number;
+  mainChart = true;
 
-  dataSeries = [
+  dataSeriesMainChart = [
     {
       data:  [],
       label: 'Distiller outflow temp'
@@ -42,13 +43,28 @@ export class AppComponent implements OnInit  {
       label: 'Radiator outflow temp'
     }
   ];
+
+  dataSeriesSecondary = [
+    {
+      data:  [],
+      label: 'Product in'
+    },
+    {
+      data: [],
+      label: 'Product out'
+    },
+    {
+      data: [],
+      label: 'Boiler'
+    }
+  ];
   chartData = [];
   chartLabels = [];
   measuredTime = '';
 
   constructor(private socketService: SocketService) {
-    this.socketService.createSocket('ws://pissbotServer:8080/ws')
-    // this.socketService.createSocket('ws://192.168.1.201:80/ws')
+    // this.socketService.createSocket('ws://pissbotServer:8080/ws')
+    this.socketService.createSocket('ws://192.168.1.201:80/ws')
       .subscribe(data => {
         try {
           data = JSON.parse(data);
@@ -64,22 +80,19 @@ export class AppComponent implements OnInit  {
               data['T2'][i] = T2;
 
               // Push data to chart series
-              this.dataSeries[0].data.push(T1);
-              this.dataSeries[2].data.push(T2);
-              this.dataSeries[1].data.push(data['setpoint'][i]);
+              this.dataSeriesMainChart[0].data.push(T1);
+              this.dataSeriesMainChart[2].data.push(T2);
+              this.dataSeriesMainChart[1].data.push(data['setpoint'][i]);
               const time = this.msToHMS(data['time'][i]);
               this.chartLabels.push(this.FormatTimeString(time[0], time[1], time[2]));
             }
             // Set current temp variables so filter continues to run without break
-            this.T1 = data['T1'][len - 1];
-            this.T2 = data['T2'][len - 1];
+            this.currentTemps[0] = data['T1'][len - 1];
+            this.currentTemps[1] = data['T2'][len - 1];
           } else {
             // If already connected, server sends one sample of data as [temp, setpoint, time]
             this.updateCurrentVals(data);
-            this.chartLabels.push(this.measuredTime);
-            this.dataSeries[0].data.push(this.T1);
-            this.dataSeries[1].data.push(this.setpoint);
-            this.dataSeries[2].data.push(this.T2);
+            this.updateChart();
           }
         } catch (err) {
           console.log(`Failed string ${data}`);
@@ -87,8 +100,8 @@ export class AppComponent implements OnInit  {
       });
   }
 
-  // Chart config
-  chartOptions = {
+  // Main chart config
+  chartOptionsMain = {
     responsive: true,
     animation: false,
     cubicInterpolationMode: 'monotone',
@@ -136,26 +149,99 @@ export class AppComponent implements OnInit  {
     legend: {
       display: true,
       position: 'top'
+    },
+    title: {
+      display: true,
+      text: "Main Process",
+      fontSize: 22
+    }
+  };
+
+  // Secondary chart config
+  chartOptionsSecondary = {
+    responsive: true,
+    animation: false,
+    cubicInterpolationMode: 'monotone',
+    downsample: {
+      enabled: true,
+      threshold: 50,
+      preferOriginalData: false,
+    },
+    elements: {
+      line: {
+        fill: false,
+      },
+      point: {
+        radius: 0
+      }
+    },
+    scales: {
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          fontSize: 22,
+          labelString: 'Temperature (°C)'
+        },
+        id: 'y-axis-1',
+        type: 'linear',
+        position: 'left',
+        ticks: {min: -10, max: 80}
+      }],
+      xAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          fontSize: 22,
+          labelString: 'Run Time'
+        },
+        ticks: {
+          maxTicksLimit: 30,
+        }
+      }]
+    },
+    gridlines: {
+      drawBorder: true
+    },
+    legend: {
+      display: true,
+      position: 'top'
+    },
+    title: {
+      display: true,
+      text: "Auxilliary Data",
+      fontSize: 22
     }
   };
 
   updateChart() {
+    this.chartLabels.push(this.measuredTime);
+    this.dataSeriesMainChart[0].data.push(this.currentTemps[0]);
+    this.dataSeriesMainChart[1].data.push(this.setpoint);
+    this.dataSeriesMainChart[2].data.push(this.currentTemps[1]);
+    this.dataSeriesSecondary[0].data.push(this.currentTemps[2]);
+    this.dataSeriesSecondary[1].data.push(this.currentTemps[3]);
+    this.dataSeriesSecondary[2].data.push(this.currentTemps[4]);
     this.chart.chart.update();
   }
 
   updateCurrentVals(data) {
-    this.T1 = this.T1 * 0.75 + parseFloat(data[0]) * 0.25;
-    this.T2 = this.T2 * 0.75 + parseFloat(data[1]) * 0.25;
-    this.setpoint = data[2];
-    const time = this.msToHMS(data[3]);
+    console.log(data);
+    this.currentTemps[0] = this.currentTemps[0] * 0.75 + parseFloat(data[0]) * 0.25;
+    this.currentTemps[1] = this.currentTemps[1] * 0.75 + parseFloat(data[1]) * 0.25;
+    this.currentTemps[2] = this.currentTemps[2] * 0.75 + parseFloat(data[2]) * 0.25;
+    this.currentTemps[3] = this.currentTemps[3] * 0.75 + parseFloat(data[3]) * 0.25;
+    this.currentTemps[4] = this.currentTemps[4] * 0.75 + parseFloat(data[4]) * 0.25;
+    this.setpoint = data[5];
+    const time = this.msToHMS(data[6]);
     this.measuredTime = this.FormatTimeString(time[0], time[1], time[2]);
-    this.flowRate = data[4];
-    this.elementStatus = data[5];
-    this.P_gain = data[6];
-    this.I_gain = data[7];
-    this.D_gain = data[8];
-    this.Qdot = this.flowRate / 60 * 4.18 * (this.T1 - this.T2);
-    this.deltaT = this.T1 - this.T2;
+    this.flowRate = data[7];
+    this.elementStatus = data[8];
+    this.P_gain = data[9];
+    this.I_gain = data[10];
+    this.D_gain = data[11];
+    this.Qdot = this.flowRate / 60 * 4.18 * (this.currentTemps[0] - this.currentTemps[1]);
+    this.deltaT = this.currentTemps[0] - this.currentTemps[1];
   }
 
   msToHMS(seconds) {
@@ -186,6 +272,7 @@ export class AppComponent implements OnInit  {
 
   receiveControllerParams($event) {
     let message: string = $event;
+    console.log(message);
     const dataArray = message.split(',');
     if (dataArray[0] === '-1') {
       dataArray[0] = this.P_gain.toString();
@@ -237,10 +324,8 @@ export class AppComponent implements OnInit  {
     this.socketService.sendMessage(message);
   }
 
-  ngOnInit() {
-    setInterval(() => {
-      this.updateChart();
-    }, 50);
+  swapCharts() {
+    this.mainChart = !this.mainChart;
   }
 }
 
